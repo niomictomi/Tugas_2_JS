af5122b715f55fe895cd88fe688370c03252f28d
niomictomi <niomic.tomi@gmail.com>
2019-08-13 20:41:38 +0700

    Tugas_2_JS

---
+++ index.js
@@ -0,0 +1,15 @@
+// TUGAS 2 JS
+
+
+var number  = 2019;
+var string  = "SEMBILAN";
+var bool    = false;
+var nool    = null;
+var arey    = [1,2,3,4,5,6,7,8,9,10];
+
+
+console.log(number);
+console.log(string);
+console.log(bool);
+console.log(nool);
+console.log(arey[2]);
